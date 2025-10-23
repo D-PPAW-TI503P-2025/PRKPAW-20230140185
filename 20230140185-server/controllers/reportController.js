deb193bd45d31e9d65b0bc68a262ba5e863a8898
ridwanfahmi <ridwanfahmidina12@gmail.com>
2025-10-23 16:25:33 +0700

    menyelesaikan tugas 4

--- 20230140185-server/controllers/reportController.js
+++ 20230140185-server/controllers/reportController.js
@@ -1,8 +1,28 @@
-const presensiRecords = require("../data/presensiData");
-exports.getDailyReport = (req, res) => {
-  console.log("Controller: Mengambil data laporan harian dari array...");
-  res.json({
-    reportDate: new Date().toLocaleDateString(),
-    data: presensiRecords,
-  });
+const { Presensi, Sequelize } = require("../models");
+
+exports.getDailyReport = async (req, res) => {
+  try {
+    console.log("Controller: Mengambil data laporan harian dari database...");
+
+    const today = new Date().toISOString().split("T")[0];
+
+    const presensiRecords = await Presensi.findAll({
+      where: Sequelize.where(
+        Sequelize.fn("DATE", Sequelize.col("checkIn")),
+        today
+      ),
+      order: [["checkIn", "ASC"]],
+    });
+
+    res.status(200).json({
+      reportDate: today,
+      data: presensiRecords,
+    });
+  } catch (error) {
+    console.error("Gagal mengambil data presensi:", error);
+    res.status(500).json({
+      message: "Terjadi kesalahan saat mengambil data laporan harian",
+      error: error.message,
+    });
+  }
 };
---
+++ Tugas/Tugas4.md
@@ -0,0 +1,13 @@
+# Tugas 4
+
+Tampilan Check-in:
+![Tampilan Check-in](./ss4/checkin.png)
+
+Tampilan Check-out:
+![Tampilan Check-out](./ss4/checkout.png)
+
+Tampilan Database:
+![Tampilan Database](./ss4/database.png)
+
+Tampilan Reports:
+![Tampilan Reports](./ss4/reports.png)
